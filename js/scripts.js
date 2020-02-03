af1965561fd286bddca05bd88761ba9c8dc8a76f
Mollydj <55717647+Mollydj@users.noreply.github.com>
2020-02-02 23:39:50 -0500

    Comments and format updated

--- js/scripts.js
+++ js/scripts.js
@@ -36,9 +36,7 @@ var pokemonRepository = (function () {
 
     var $modal = $('<div class="modal"></div>');
     var $closeButtonElement = $('<button class="modal-close">Close</button>');
-    $closeButtonElement.on('click', function (event) {
-      hideModal();
-    });
+    $closeButtonElement.on('click', function (event) {hideModal();} );
 
     var $titleElement = $('<h1>' + item.name + '</h1>');
     var $imageElement = $('<img src=" ' +item.imageUrl+ ' " class="modal-img">');
@@ -53,69 +51,66 @@ var pokemonRepository = (function () {
   }
 
   //Hide Modal & Event Listeners
-    function hideModal() {
-    $modalContainer.removeClass('is-visible');
-    }
-
-    $(document).keydown(function (e) {  if (e.key === "Escape") { hideModal();} });
-
-    $modalContainer.click(function() {  hideModal();});
+  function hideModal() {  $modalContainer.removeClass('is-visible');}
+  $(document).keydown(function (e) {  if (e.key === "Escape") { hideModal();} });
+  $modalContainer.click(function() {hideModal();} );
 
 
-    //Function to add new pokemon data
-    function add(name) {
-      repository.push(name);
-    }
+  //Function to add new pokemon data
+  function add(name) {
+    repository.push(name);
+  }
 
-function loadList() {
-  return $.ajax(apiUrl, { dataType: 'json' })
-  .then(function(item) {
-    $.each(item.results, function(index, item) {
-      var pokemon = {
-        name: item.name,
-        detailsUrl: item.url
-      };
-      add(pokemon);
+//Function to load API items
+  function loadList() {
+    return $.ajax(apiUrl, { dataType: 'json' })
+    .then(function(item) {
+      $.each(item.results, function(index, item) {
+        var pokemon = {
+          name: item.name,
+          detailsUrl: item.url
+        };
+        add(pokemon);
+      });
+    })
+    .catch(function(error) {
+      /*Load Functions Set In Order To Retrieve Data From Pokemon API*/
+      document.write(error);
     });
-  })
-  .catch(function(error) {
-    /*Load Functions Set In Order To Retrieve Data From Pokemon API*/
-    document.write(error);
-  });
-}
+  }
 
-function loadDetails(item) {
-  var url = item.detailsUrl;
-  return $.ajax(url, {dataType: 'json'}).then(function(responseJSON) {
-    return responseJSON;
-  }).then(function (details) {
-    item.imageUrl = details.sprites.front_default;
-    item.height = details.height;
-    item.weight = details.weight;
-    item.types = Object.keys(details.types);
-  }).catch(function (e) {
-    console.error(e);
-  });
-}
+// Function to load pokedex details
+  function loadDetails(item) {
+    var url = item.detailsUrl;
+    return $.ajax(url, {dataType: 'json'}).then(function(responseJSON) {
+      return responseJSON;
+    }).then(function (details) {
+      item.imageUrl = details.sprites.front_default;
+      item.height = details.height;
+      item.weight = details.weight;
+      item.types = Object.keys(details.types);
+    }).catch(function (e) {
+      console.error(e);
+    });
+  }
 
-return {
-  add: add,
-  getAll: getAll,
-  addListItem: addListItem,
-  showDetails: showDetails,
-  loadList: loadList,
-  loadDetails: loadDetails,
-  //showModal: showModal,
-  hideModal: hideModal
-};
-
+//Return all previous functions to call outside of IIFE
+  return {
+    add: add,
+    getAll: getAll,
+    addListItem: addListItem,
+    showDetails: showDetails,
+    loadList: loadList,
+    loadDetails: loadDetails,
+    showModal: showModal,
+    hideModal: hideModal
+  };
 
 })();
 
-
-
-
+//Creates list of Pokemon with Pokemon's name on the button
 pokemonRepository.loadList().then(function() {
+//Pokemon details loaded
   pokemonRepository.getAll().forEach(function(pokemon){
     pokemonRepository.addListItem(pokemon);
   });
